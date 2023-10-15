d6ed1a8d238776c3929d22bbcecc857fb2d60433
Your Name <rebal.wehbe@gmail.com>
2023-10-15 13:06:23 -0700

    66666

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %id byte(s)\n", sizeof(char));
-printf("Size of an int: %id byte(s)\n", sizeof(int));
-printf("Size of a long int: %id byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %id byte(s)\n", sizeof(double));
-printf("Size of a float: %id byte(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(double));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
