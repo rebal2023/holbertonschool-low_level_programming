242ad8201b1f108ddd393cae91258a071c250ab7
Your Name <rebal.wehbe@gmail.com>
2023-10-15 12:51:18 -0700

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %id byte(s)\n", sizeof(char));
+  printf("Size of an int: %id byte(s)\n", sizeof(int));
+  printf("Size of a long int: %id byte(s)\n", sizeof(long int));
+  printf("Size of a long long int: %id byte(s)\n", sizeof(double));
+  pintf("Size of a float: %id byte(s)\n", sizeof(float));
+  return (0);
+}
