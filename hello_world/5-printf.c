996b4866b4ca5cc28b216fa9a4e439ef6a57de6c
Your Name <rebal.wehbe@gmail.com>
2023-10-15 13:12:23 -0700

    555

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-/*
-*main print
-*return 0
-*/
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
