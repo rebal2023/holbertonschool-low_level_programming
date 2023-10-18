158c3cd13c8e6dbf5a68aea0aad76b913d2eb58e
Your Name <rebal.wehbe@gmail.com>
2023-10-18 08:08:17 -0700

    99

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,20 +10,12 @@ int number = 0;
 
 while (number < 10)
 {
-if (number != 9)
-{
 putchar(number + '0');
 putchar(',');
 putchar(' ');
 number++;
 }
-else
-{
-putchar(number + '0');
-number++;
-}
-}
-
+ 
 putchar('\n');
 
 return (0);
