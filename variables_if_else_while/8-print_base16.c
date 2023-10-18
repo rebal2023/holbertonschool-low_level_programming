07985b999e70960805825a59b769028ac65706c8
Your Name <rebal.wehbe@gmail.com>
2023-10-18 07:05:41 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int number = 0;
+char letter = 'a';
+while (number < 10)
+{
+putchar(number + '0');
+number++;
+}
+while (letter <= 'f')
+{
+putchar(letter);
+letter ++;
+}
+
+putchar('\n');
+
+return (0);
+}
