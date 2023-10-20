40f250862b68758b2f9f41aba85f11be01fcd5e6
Your Name <rebal.wehbe@gmail.com>
2023-10-20 00:59:16 -0700

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 int number1 = 0;
 int number2 = 1;
 
-while (number1 < 10)
+while ( number1 <10)
 {
-while (number2 < 10)
+while (number1 != number2 && number2 < 10)
 {
 if (number1 < number2)
 {
@@ -22,8 +22,8 @@ putchar(' ');
 }
 number2++;
 }
-number2 = 1;
 number1++;
+number2 = number1 + 1;
 }
 putchar('\n');
 return (0);
