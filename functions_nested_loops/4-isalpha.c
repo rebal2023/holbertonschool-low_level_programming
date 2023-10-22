097d6c3fb0f351faf89e038977fec89fed488e15
Your Name <rebal.wehbe@gmail.com>
2023-10-22 02:01:03 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isalpha - checks for alphabetic character
+*c - variable
+*Return: 1 or 0
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
