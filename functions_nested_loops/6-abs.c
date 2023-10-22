99d1deb940290132277521ab406783aacb322504
Your Name <rebal.wehbe@gmail.com>
2023-10-22 02:38:31 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_abs - computes the absolute value of an integer
+*@c: integer to be checked
+*Return: 0
+*/
+int _abs(int c)
+{
+if (c < 0)
+{
+c = c * -1;
+return (c);
+}
+else
+{
+return (c);
+}
+}
