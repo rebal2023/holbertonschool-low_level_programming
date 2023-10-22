629b52598bbaf70f88c2d3a74fba90cff1106012
Your Name <rebal.wehbe@gmail.com>
2023-10-22 13:58:58 -0700

    100

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -2,13 +2,16 @@
 #include "main.h"
 /**
 * print_number - function that prints an integer
+*@n: variable
 */
 void print_number(int n)
 {
-  if (n < 0) {
+  if (n < 0)
+  {
     _putchar('-');
     n = -n;
   }
+  
   if (n / 10) {
     print_number(n / 10);
   }
