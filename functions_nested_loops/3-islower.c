ac5119ac66bebd10d8f8c42753712f112fe226c2
Your Name <rebal.wehbe@gmail.com>
2023-10-22 01:50:42 -0700

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,21 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_islower - checks for lowercase character
- * *
- * *Return: 0
- * */
+*_islower - checks for lowercase character
+*
+*Return: 1 or 0
+*/
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-_putchar('1');
-return (0);
+return (1);
 }
 else
 {
-_putchar('0');
 return(0);
 }
 }
-
