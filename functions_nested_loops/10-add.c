801d1b5adf339022dcd565346d5d75af043cd787
Your Name <rebal.wehbe@gmail.com>
2023-10-22 06:30:16 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*add - adds two integers and returns the result
+*@n1: first integer
+*@n2: second integer
+*Return: int
+*/
+int add(int n1, int n2)
+{
+  int sum;
+  sum = n1 + n2;
+  return (sum);
+}
