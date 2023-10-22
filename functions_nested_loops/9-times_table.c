9397ac7ad61d30b7e2b22032d1b712352ae65585
Your Name <rebal.wehbe@gmail.com>
2023-10-22 06:06:26 -0700

    9999

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * times_table - function that prints the 9 times table
- */
+* times_table - function that prints the 9 times table
+*/
 void times_table(void)
 {
-  int x;
-  int y;
+int x;
+int y;
 
-  for (x = 0; x <= 9; x++)
-    {
-      for (y = 0; y <= 9; y++)
-	{
-	  int xy = x * y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+int xy = x * y;
 
-
-	  if (y == 0)
-	    {
-	      _putchar((xy) + '0');
-	      _putchar(',');
-	    }
-	  else if (xy <= 9)
-	    {
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar((xy) + '0');
-	      _putchar(',');
-	    }
-	  else if (xy > 9)
-	    {
-	      _putchar(' ');
-	      _putchar((xy / 10) + '0');
-	      _putchar((xy % 10) + '0');
-	      _putchar(',');
-	    }
-	}
-      _putchar('\n');
-    }
-  _putchar('\n');
+if (y == 0)
+{
+_putchar((xy) + '0');
+_putchar(',');
+}
+else if (xy <= 9)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((xy) + '0');
+_putchar(',');
+}
+else if (xy > 9)
+{
+if (x == 9 && y == 9)
+{
+_putchar((xy / 10) + '0');
+_putchar((xy % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((xy / 10) + '0');
+_putchar((xy % 10) + '0');
+_putchar(',');
+}
+}
+}
+_putchar('\n');
+}
+_putchar('\n');
 }
