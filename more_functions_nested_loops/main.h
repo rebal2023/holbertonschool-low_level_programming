78e9d809748c782fc9dcc43c8098e6fd35e36bcc
Your Name <rebal.wehbe@gmail.com>
2023-10-22 11:52:03 -0700

    main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,8 @@
-idigit(int c);
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -8,3 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 int main(void);
 void print_triangle(int size);
+#endif
