68552344bdbd8a08ea714365ac9ea9ee94f4cc94
Your Name <rebal.wehbe@gmail.com>
2023-10-22 07:02:44 -0700

    11--

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,35 @@
 void print_to_98(int n)
 {
 int i;
-
-if (n == 98)
-{
-  printf("%d", n);
-}
  
- else if (n < 98)
-    {
-      for (i = n; i < 99; i++)
-	{
-	      printf("%d, ", i);
-	}
-      printf("\n");
-    }
- else
-    {
-      for (i = n; i > 97; i--)
-	{
-	      printf("%d, ", i);
-	}
-      printf("\n");
-    }
+if (n < 99)
+{
+for (i = n; i < 99; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else if (i != 99)
+{
+printf("%d, ", i);
+}
+}
+printf("\n");
+}
+else
+{
+for (i = n; i >= 97; i--)
+{
+if (i == 98)
+{
+printf("%i", i);
+}
+else if (d != 97)
+{
+printf("%d, ", i);
+}
+}
+printf("\n");
+}
 }
