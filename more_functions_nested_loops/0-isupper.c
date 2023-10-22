d9133ae17ce1e0e3fb23c9006b66ff376ef68f88
Your Name <rebal.wehbe@gmail.com>
2023-10-22 11:53:55 -0700

    000

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_isupper - function
- *@c: variable
- *Return: 1 or 0
- */
+*_isupper - function
+*@c: variable
+*Return: 1 or 0
+*/
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else if (c >= 'a' && c <= 'z')
-    {
-      return (0);
-    }
-  return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+return (0);
 }
