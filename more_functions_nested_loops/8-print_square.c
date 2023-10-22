b8439efcfac43ee9b2bb76196de1b3133e710e5a
Your Name <rebal.wehbe@gmail.com>
2023-10-22 13:26:01 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_square - prints a square
+*@size: variable
+*/
+void print_square(int size)
+{
+int x;
+int y;
+
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size <= 0)
+{
+ _putchar('\n');
+}
+}
