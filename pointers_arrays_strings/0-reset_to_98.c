3c5f52cd88e4a1f12d8575902bea6b5692704cec
Your Name <rebal.wehbe@gmail.com>
2023-10-27 08:13:07 -0700

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+  int *n;
+
+  printf("n=%d\n", n);
+
+  *n = 98;
+
+  printf("n=%d\n", n);
+}
