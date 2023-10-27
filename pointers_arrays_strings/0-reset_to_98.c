d7d7f79db96ba89cb20ee57716eef50f4a574ec8
Your Name <rebal.wehbe@gmail.com>
2023-10-27 08:33:17 -0700

    98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -7,13 +7,15 @@
 
 void reset_to_98(int *n)
 {
-  _putchar('n=');
+  _putchar('n');
+  _putchar('=');
   _putchar(*n);
   _putchar('\n');
 
   *n = 98;
 
-  _putchar('n=');
+  _putchar('n');
+  _putchar('=');
   _putchar(*n);
   
 }
