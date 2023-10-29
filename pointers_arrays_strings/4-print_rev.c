6c23f13c2e2af619c2cafb64b4896adb8511f6b5
Your Name <rebal.wehbe@gmail.com>
2023-10-29 11:13:59 -0700

    printreverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_rev - prints a string in reverse
+*@s: string
+*/
+void print_rev(char *s)
+{
+int x = strlen(s) - 1;
+while (x >= 0) {
+_putchar(s[x]);
+i--;
+}
+_putchar('\n');
+}
