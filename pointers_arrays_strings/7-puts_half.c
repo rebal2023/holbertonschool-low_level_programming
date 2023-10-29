55dc58eb711431e90f325fa9efd4f9e87771935f
Your Name <rebal.wehbe@gmail.com>
2023-10-29 14:01:04 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts_half - prints half of a string
+* @str: string
+*/
+
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	  {
+		len++;
+	  }
+	for (i = (len/2); str[i] != '\0'; i++)
+	  {
+	    _putchar(str[i]);
+	  }
+	_putchar('\n');
+}
