6941069d04a5856e2bb7152fac76ce5d8671c8a9
Your Name <rebal.wehbe@gmail.com>
2023-10-29 14:16:13 -0700

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,15 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i <= (n - 1); i++)
-	{
-		printf("%d, ", a[i]);
-	}
-
-	printf("\n");
+for (i = 0; i <=(n - 1); i++)
+{
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
+}
+else
+{ 
+printf("%d, ", a[i]);
+}
+printf("\n");
 }
