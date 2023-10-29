228221bb4e75832160dcdad0c619a70b3d83f436
Your Name <rebal.wehbe@gmail.com>
2023-10-29 10:44:00 -0700

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,15 @@
 /**
 *_strlen - returns the length of a string
 *@s: string
-*return : value
+*Return : value
 */
 int _strlen(char *s)
 {
 int i = 0;
-while(*s != '\0')
+while (*s != '\0')
 {
 i++;
 s++;
 }
-return i;
+return (i);
 }
