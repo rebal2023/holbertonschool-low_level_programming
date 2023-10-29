0bfd764f8e335dbd09646e942877c00898e008a7
Your Name <rebal.wehbe@gmail.com>
2023-10-29 13:50:50 -0700

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts2 -  prints every other character of a string
- * @str: input
- */
+* puts2 -  prints every other character of a string
+* @str: string
+*/
 void puts2(char *str)
 {
-	int i = 0;
+int i = 0;
 _putchar(str[i]);
-	while (str[i] != '\0')
-	{
-	  ++i;
-	  ++i;
-			_putchar(str[i]);
-	}
-	_putchar('\n');
+while (str[i] != '\0')
+{
+++i;
+++i;
+_putchar(str[i]);
+}
+_putchar('\n');
 }
