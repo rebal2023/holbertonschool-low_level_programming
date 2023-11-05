a24c92757b25efd1075bef64365a9b1bb4320b9c
Your Name <rebal.wehbe@gmail.com>
2023-11-05 01:56:14 -0800

    101

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_number - function that prints an integer
+* @n: integer params
+* Return: 0
+*/
+void print_number(int n)
+{
+unsigned int m;
+
+m = n;
+
+if (n < 0)
+{
+_putchar('-');
+m = -n;
+}
+if (m / 10 != 0)
+{
+print_number(m / 10);
+}
+_putchar((m % 10) + '0');
+}
