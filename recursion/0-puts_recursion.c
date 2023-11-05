ca5f8f7be8ccebdddb03f6a2ab13ebc5b7c4a33d
Your Name <rebal.wehbe@gmail.com>
2023-11-05 13:17:49 -0800

    0recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts_recursion - function that prints a string
+* @s: variable
+* Return: 0.
+*/
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
