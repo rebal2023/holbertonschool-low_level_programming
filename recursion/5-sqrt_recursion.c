90993934345b41f90536761781efd9823de4fc5a
Your Name <rebal.wehbe@gmail.com>
2023-11-05 13:42:21 -0800

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_sqrt - function
+*@n: integer
+*@i: integer
+*Return: integer
+*/
+int _sqrt(int x, int y)
+{
+if (x == 0)
+{
+return (1);
+}
+else if (x < 0)
+{
+return (-1);
+}
+else if (y * y == n)
+{
+return (y);
+}
+else
+{
+return (_sqrt(x, y + 1));
+}
+}
+/**
+*_sqrt_recursion - function that returns the natural square root of a nb
+*@n: integer
+*Return: integer
+*/
+int _sqrt_recursion(int n)
+{
+return (_sqrt(x, 1));
+}
