d75206ca1875f68277529a16442004be81aa0739
Your Name <rebal.wehbe@gmail.com>
2023-11-05 01:13:04 -0800

    lowertoupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*string_toupper - function that changes all lowercase letters of a string to uppercase
+*@s: variable
+*Return: char
+*/
+char *string_toupper(char *s)
+{
+int x = 0;
+
+while (s[x] != '\0')
+{
+if (s[x] >= 'a' && s[x] <= 'z')
+{
+s[x] -= 32;
+}
+x++;
+}
+return (s);
+}
