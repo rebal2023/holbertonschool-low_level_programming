49e206f1235cd8c2c80b608fff7a97cfe4a8f32c
Your Name <rebal.wehbe@gmail.com>
2023-11-09 14:09:31 -0800

    palindrome

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -5,7 +5,7 @@ int p(char *s, int x, int len);
 int len(char *s);
 
 /**
-* is_palindrome - function that returns 1 if a string is a palindrome and 0 if not
+* is_palindrome - function returns 1 if string is palindrome & 0 if not
 * @s: string
 * Return: 0 or 1
 */
