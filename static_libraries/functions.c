22793ff81efdd34d3717c0e7eefde5ab62b44d9c
Your Name <rebal.wehbe@gmail.com>
2023-11-12 10:41:41 -0800

    static

---
+++ static_libraries/functions.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+int _putchar(char c) {
+  return
+ 
+_putchar(c);
+}
+
+int _islower(int c) {
+  return
+ 
+_islower(c);
+}
+
+int _isalpha(int c) {
+  return
+ 
+_isalpha(c);
+}
+
+int _abs(int n) {
+  return
+ 
+_abs(n);
+}
+
+int _isupper(int c) {
+  return
+ 
+_isupper(c);
+}
+
+int _isdigit(int c) {
+  return
+ 
+_isdigit(c);
+}
+
+int _strlen(char *s) {
+  return
+ 
+_strlen(s);
+}
+
+void _puts(char *s) {
+  _puts(s);
+}
+
+char *_strcpy(char *dest, char *src) {
+  return
+ 
+_strcpy(dest, src);
+}
+
+int _atoi(char *s) {
+  return _atoi(s);
+}
+
+char *_strcat(char *dest, char *src) {
+  return
+ 
+_strcat(dest, src);
+}
+
+char *_strncat(char *dest, char *src,
+ 
+int n) {
+  return
+ 
+_strncat(dest, src, n);
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+  return
+ 
+_strncpy(dest, src, n);
+}
+
+int _strcmp(char *s1, char *s2) {
+  return
+ 
+_strcmp(s1, s2);
+}
+
+char *_memset(char *s, char b, unsigned
+ 
+int n) {
+  return
+ 
+_memset(s, b, n);
+}
+
+char *_memcpy(char *dest, char *src, unsigned
+ 
+int n) {
+  return
+ 
+_memcpy(dest, src, n);
+}
+
+char *_strchr(char *s, char c) {
+  return
+ 
+_strchr(s, c);
+}
+
+unsigned
+ 
+int _strspn(char *s, char *accept) {
+  return
+ 
+_strspn(s, accept);
+}
+
+char *_strpbrk(char *s, char *accept) {
+  return
+ 
+_strpbrk(s, accept);
+}
+
+char *_strstr(char *haystack, char *needle) {
+  return
+ 
+_strstr(haystack, needle);
+}
