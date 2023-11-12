71212517167061cd1930c3f9e221203572214ee9
Your Name <rebal.wehbe@gmail.com>
2023-11-12 10:21:32 -0800

    101

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* wildcmp - Compare two strings
+* @s1: pointer of string 1
+* @s2: pointer of string 2
+* Return: 0 or 1
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
+}
