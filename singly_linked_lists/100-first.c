9d2a1c78cba889be215615846ab1c95096856713
rebal2023 <rebal.wehbe@gmail.com>
2023-12-08 08:31:46 -0800

    100

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - print before main
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
