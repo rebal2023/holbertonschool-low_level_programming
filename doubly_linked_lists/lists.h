212cd6703abd9482d982d134e1e89a4cfdd25f67
rebal2023 <rebal.wehbe@gmail.com>
2023-12-10 08:46:12 -0800

    0

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -11,9 +11,9 @@
 */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *pre;
-	struct dlistint_s *post;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
