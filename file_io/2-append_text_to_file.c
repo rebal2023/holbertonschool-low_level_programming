608b27b93e46306ecc216cc4c1b7d28b0a379153
rebal2023 <rebal.wehbe@gmail.com>
2023-12-17 13:41:32 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 /**
 * append_text_to_file - function that appends text at the end of a file
 * @filename: name of the file
-* @text_content: string to add at the end of the file 
+* @text_content: string to add at the end of the file
 * Return: 1 or-1
 */
 int append_text_to_file(const char *filename, char *text_content)
